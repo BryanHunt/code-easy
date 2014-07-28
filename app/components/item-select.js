23dd96c23cb9f87cb749ccc11be20c9b9925e215
Bryan Hunt <bhunt@mac.com>
2014-07-28 12:31:59 -0500

    Initialize the select

--- app/components/item-select.js
+++ app/components/item-select.js
@@ -3,20 +3,29 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'span',
   items: {},
+  choices: [],
 
   itemsChanged: function() {
     var self = this;
     this.set('choices', Ember.A());
 
-    this.get('items').then(function(items){    
-      items.forEach(function(item){
-        var label = item.get(self.get('labelPath'));
-        var value = item.get(self.get('valuePath'));
-        self.set('items.' + value, item);
-        self.get('choices').addObject(Ember.Object.create({label: label, value: value}));
-      });
+    this.get('selected').then(function(selected) {
+      self.get('items').then(function(items){    
+        items.forEach(function(item){
+          var label = item.get(self.get('labelPath'));
+          var value = item.get(self.get('valuePath'));
+          var initSelected;
+
+          if (item === selected) {
+            initSelected = "selected";
+          }
+
+          self.set('items.' + value, item);
+          self.get('choices').addObject(Ember.Object.create({label: label, value: value, selected: initSelected}));
+        });
+      });      
     });
-  }.observes('items').on('init'),
+   }.observes('items').on('init'),
 
   change: function(e) {
     this.set('selected', this.get('items.' + e.target.value));
