94164547c157e7a1fc86bea67e4e9c4aaf117dab
Bryan Hunt <bhunt@mac.com>
2014-08-09 15:50:53 -0500

    Add initial support for changing the css

--- app/controllers/experimental.js
+++ app/controllers/experimental.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: 'application',
+  styles: Ember.computed.alias("controllers.application.model"),
+
   types: function(){
     return this.store.find('ast/type');
-  }.property()
+  }.property(),
+
+  actions: {
+    applyStyles: function() {
+      this.get('controllers.application').applyStyles();
+    }
+  }
 });
--- server/routes/css/styles.js
+++ server/routes/css/styles.js
@@ -4,11 +4,30 @@ module.exports = function(app) {
   var properties = store.create();
 
   var s1 = {
+    selector: '.keyword-class',
+    properties: [1]
+  };
+
+  var s2 = {
     selector: '.function-body',
-    properties: []
+    properties: [2]
   };
 
   styles.addObject(s1);
+  styles.addObject(s2);
+
+  var p1 = {
+    name: "color",
+    value: "red"
+  };
+
+  var p2 = {
+    name: "margin-left",
+    value: "25px"
+  };
+
+  properties.addObject(p1);
+  properties.addObject(p2);
 
   var express = require('express');
   var cssStylesRouter = express.Router();
