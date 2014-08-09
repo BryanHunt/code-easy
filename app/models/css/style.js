a8c6e000f0560941022e95a5769d9c75038b8a2e
Bryan Hunt <bhunt@mac.com>
2014-08-09 17:15:09 -0500

    Style tweakage

--- app/models/css/style.js
+++ app/models/css/style.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  label: DS.attr('string'),
   selector: DS.attr('string'),
   properties: DS.hasMany('css/property')
 });
--- server/routes/css/styles.js
+++ server/routes/css/styles.js
@@ -4,17 +4,33 @@ module.exports = function(app) {
   var properties = store.create();
 
   var s1 = {
+    label: 'Class Keyword',
     selector: '.keyword-class',
     properties: [1]
   };
 
   var s2 = {
-    selector: '.function-body',
+    label: 'Parameter Type',
+    selector: '.parameter-type',
     properties: [2]
+  };
+
+  var s3 = {
+    label: 'Function Body',
+    selector: '.function-body',
+    properties: [3]
+  };
+
+  var s4 = {
+    label: 'Return Keyword',
+    selector: '.keyword-return',
+    properties: [4]
   };
 
   styles.addObject(s1);
   styles.addObject(s2);
+  styles.addObject(s3);
+  styles.addObject(s4);
 
   var p1 = {
     name: "color",
@@ -22,12 +38,24 @@ module.exports = function(app) {
   };
 
   var p2 = {
+    name: "color",
+    value: "red"
+  };
+
+  var p3 = {
     name: "margin-left",
     value: "25px"
   };
 
+  var p4 = {
+    name: "color",
+    value: "red"
+  };
+
   properties.addObject(p1);
   properties.addObject(p2);
+  properties.addObject(p3);
+  properties.addObject(p4);
 
   var express = require('express');
   var cssStylesRouter = express.Router();
